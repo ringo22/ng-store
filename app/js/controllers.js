af2b335ca1231c2e880636b4cf69798122934129
Rodrigo Raya <rod@MacBook-Pro-de-Rodrigo.local>
2014-08-08 18:13:49 -0500

    Add banners in home.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,5 +4,8 @@
 var storeControllers = angular.module('storeControllers', []);
 
 storeControllers.controller('GetBanners', ['$scope', 'Banner', function($scope, Banner) {
-	$scope.banners = Banner.query();
+	$scope.banners_H = Banner.query({ position: 'H' });
+	$scope.banners_S = Banner.query({ position: 'S' });
+	$scope.banners_P = Banner.query({ position: 'P' });
+	$scope.banners_R = Banner.query({ position: 'R' });
 }]);
--- app/js/services.js
+++ app/js/services.js
@@ -6,6 +6,6 @@ var storeServices = angular.module('storeServices', ['ngResource']);
 
 storeServices.factory('Banner', ['$resource', function($resource) {
   return $resource('https://www.easy-shop.com/com/helpers.cfc', {}, {
-    query: { method:'GET', params:{ method: 'getBanners', position: 'P' }, isArray:true }
+    query: { method:'GET', params:{ method: 'getBanners', position: ':position' }, isArray:true }
   });
 }]);
